93a31477f6d4916050cb23026e4f85505d146edd
chenyibin <cheyibin@showpay.top>
2021-09-07 15:56:28 +0800

    update classifyList

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -646,7 +646,7 @@ async function createNft() {
     nftissuerName: store.state.userInfo!.name,
     content: {
       nftType: nft.type,
-      classifyList: JSON.stringify(nft.classify),
+      classifyList: nft.classify,
       originalFileTxid: {
         fileType: originalFile.data_type,
         fileName: originalFile.name,
