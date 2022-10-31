31eb08b39431a7cdf21d6bf0596240f03de66a8a
CallMeSoul <callmesoul@foxmail.com>
2022-10-31 18:48:37 +0800

    添加metamask 登录

---
+++ src/components/ConnectWalletModal/ConnectWalletModal.vue
@@ -0,0 +1,7 @@
+<template>
+  <ElDialog :model-value="true" center :title="$t('connectWallet')"></ElDialog>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped src="./ConnectWalletModal.scss"></style>
