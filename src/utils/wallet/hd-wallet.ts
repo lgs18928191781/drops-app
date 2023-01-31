ca180f07840971a8dcf184f1762cb8a5015c8c3c
lgs <lgs18924946920@showpay.top>
2023-01-31 16:30:46 +0800

    修复拿钱接口

--- src/utils/wallet/hd-wallet.ts
+++ src/utils/wallet/hd-wallet.ts
@@ -95,6 +95,7 @@ export interface MetaNameRequestDate {
 }
 
 export interface BaseUserInfoTypes {
+  accessKey?: string
   userType: string
   name: string
   phone: string
@@ -635,7 +636,7 @@ export class HdWallet {
               const initUtxo = await this.provider.getInitAmount({
                 address: this.rootAddress,
                 xpub: this.wallet.xpubkey.toString(),
-                token: account.token || '',
+                token: account.token || account.accessKey || '',
                 userName: account.userType === 'phone' ? account.phone : account.email,
               })
               utxos = [initUtxo]
