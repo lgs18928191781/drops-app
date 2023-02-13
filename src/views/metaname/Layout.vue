b8c091959093cc2d629ca93f94891f9fdfbf6e85
CallMeSoul <callmesoul@foxmail.com>
2023-02-13 16:38:14 +0800

    fix: ui bug

--- src/views/metaname/Layout.vue
+++ src/views/metaname/Layout.vue
@@ -1,10 +1,17 @@
 <template>
   <HeaderVue />
   <RouterView />
+
+  <!-- wallet -->
+  <MyWalletVue v-model="layout.isShowWallet" />
 </template>
 
 <script setup lang="ts">
+import { useLayoutStore } from '@/stores/layout'
 import HeaderVue from './components/Header/Header.vue'
+import MyWalletVue from '@/components/LoginedUserOperate/MyWallet.vue'
+
+const layout = useLayoutStore()
 </script>
 <style lang="scss"></style>
 <style lang="scss" src="./Layout.scss"></style>
