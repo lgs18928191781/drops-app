78f46a8d40eeffb63e7db1b6583e58bba0715dbb
Vincent Tse <river_run@126.com>
2023-02-22 18:09:54 +0800

    添加pwa支持

--- vite.config.js
+++ vite.config.js
@@ -103,10 +103,37 @@ export default ({ mode, command }) => {
         'mvc-lib/mnemonic': 'Mnemonic',
         bip39: 'bip39',
       }),
+
       VitePWA({
         registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'show3.svg'],
         devOptions: {
           enabled: true,
+        },
+        manifest: {
+          name: 'Show3',
+          short_name: 'Show3',
+          description:
+            "Show3 is the World's First NFT-Universal Web3 Social Application. It is the first decentralized social platform based on the DID protocol with fully on-chain data.",
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
         },
       }),
       // basicSsl(),
