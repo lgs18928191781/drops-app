43966dd315f57ef85163404a9c0090c6e22de4b8
Vincent Tse <river_run@126.com>
2023-02-23 18:51:14 +0800

    添加自定义api服务支持

--- src/utils/wallet/hd-wallet.ts
+++ src/utils/wallet/hd-wallet.ts
@@ -1569,6 +1569,7 @@ export class HdWallet {
   public getNftManager = (): NftManager => {
     const nftManager = new NftManager({
       apiTarget: API_TARGET.MVC,
+      apiHost: process.env.VITE_META_SV_API,
       // @ts-ignore
       network: this.network,
       purse: this.wallet!.deriveChild(0)
