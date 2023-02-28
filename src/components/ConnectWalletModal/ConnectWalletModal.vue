a373a55137256b04f6114d0a68339355b23aae4a
lgs <lgs18924946920@showpay.top>
2023-02-28 11:11:29 +0800

    账户升级

--- src/components/ConnectWalletModal/ConnectWalletModal.vue
+++ src/components/ConnectWalletModal/ConnectWalletModal.vue
@@ -829,6 +829,7 @@ async function connectWalletConnect(isUpdate: boolean = false) {
       })
   } else {
     try {
+      console.log('accounts[0]', accounts[0])
       address = isUpdate ? accounts[0] : accounts[0].toLocaleLowerCase()
       message = isUpdate
         ? import.meta.env.MODE == 'gray'
@@ -886,7 +887,7 @@ async function connectWalletConnect(isUpdate: boolean = false) {
     }
   }
 
-  // connector.killSession()
+  connector.killSession()
 }
 
 async function onModalClose() {
