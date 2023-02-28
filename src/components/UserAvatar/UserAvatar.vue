897c690e7dc458dad0058265adff37168f6569e9
CallMeSoul <callmesoul@foxmail.com>
2023-02-28 19:06:11 +0800

    update: 头像用户卡片显示

--- src/components/UserAvatar/UserAvatar.vue
+++ src/components/UserAvatar/UserAvatar.vue
@@ -5,7 +5,7 @@
     :type="type"
     :default-image="DefaultAvatar"
     :custom-class="customClass"
-    @click="toUserPage"
+    @click="e => openUserCard(e)"
     ref="AvatarRef"
   />
 </template>
@@ -21,6 +21,11 @@ import { createPopper, VariationPlacement } from '@popperjs/core'
 const router = useRouter()
 const userCardWarpId = `user-card-warp-${v1()}`
 const AvatarRef = ref()
+let popper: any
+let popperLeftRange = [0, 0]
+let buttonLeftRange = [0, 0]
+let buttonTopRange = [0, 0]
+let popperTopRange = [0, 0]
 
 interface Props {
   name?: string
@@ -46,25 +51,82 @@ const toUserPage = () => {
   }
 }
 
-function openUserCard() {
-  const div = document.createElement('div')
-  div.id = userCardWarpId
-  div.className = 'user-card-warp'
-  document.body.append(div)
-  render(
-    // @ts-ignore
-    h(UserCard, {
-      i18n: i18n,
-      modelValue: true,
-      metaId: props.metaId,
-      name: props.name,
-    }),
-    document.getElementById(userCardWarpId)!
-  )
-  nextTick(() => {
-    createPopper(AvatarRef.value.imgRef, document.getElementById(userCardWarpId)!, {
-      placement: 'right-start',
+function openUserCard(e: any) {
+  if (props.disabled) return
+  e.stopPropagation()
+  if (popper) {
+    destroy()
+  } else {
+    const div = document.createElement('div')
+    div.id = userCardWarpId
+    div.className = 'user-card-warp'
+    document.body.append(div)
+    render(
+      // @ts-ignore
+      h(UserCard, {
+        i18n: i18n,
+        modelValue: true,
+        metaId: props.metaId,
+        name: props.name,
+        onHide: () => {
+          destroy()
+        },
+      }),
+      document.getElementById(userCardWarpId)!
+    )
+    nextTick(() => {
+      popper = createPopper(AvatarRef.value.imgRef, document.getElementById(userCardWarpId)!, {
+        placement: 'right-start',
+      })
+      nextTick(() => {
+        const userCardWarp = document.getElementById(userCardWarpId)!
+        popperLeftRange = [
+          userCardWarp.getBoundingClientRect().left,
+          userCardWarp.getBoundingClientRect().left + userCardWarp.getBoundingClientRect().width,
+        ]
+        popperTopRange = [
+          userCardWarp.getBoundingClientRect().top,
+          userCardWarp.getBoundingClientRect().top + userCardWarp.getBoundingClientRect().height,
+        ]
+        buttonLeftRange = [
+          AvatarRef.value.imgRef.getBoundingClientRect().left,
+          AvatarRef.value.imgRef.getBoundingClientRect().left +
+            AvatarRef.value.imgRef.getBoundingClientRect().width,
+        ]
+        buttonTopRange = [
+          AvatarRef.value.imgRef.getBoundingClientRect().top,
+          AvatarRef.value.imgRef.getBoundingClientRect().top +
+            AvatarRef.value.imgRef.getBoundingClientRect().height,
+        ]
+      })
     })
-  })
+    document.addEventListener(
+      'click',
+      e => {
+        if (
+          (e.clientX > popperLeftRange[0] &&
+            e.clientX < popperLeftRange[1] &&
+            e.clientY > popperTopRange[0] &&
+            e.clientY < popperTopRange[1]) ||
+          (e.clientX > buttonLeftRange[0] &&
+            e.clientX < buttonLeftRange[1] &&
+            e.clientY > buttonTopRange[0] &&
+            e.clientY < buttonTopRange[1])
+        ) {
+          return
+        } else {
+          destroy()
+        }
+      },
+      true
+    )
+  }
+}
+
+function destroy() {
+  document.removeEventListener('click', () => {})
+  popper?.destroy()
+  popper = undefined
+  document.getElementById(userCardWarpId)?.remove()
 }
 </script>
