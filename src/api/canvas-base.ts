4c829d661aaa3b37caaf1cde36dd7e6716f482bc
CallMeSoul <callmesoul@foxmail.com>
2023-03-03 15:13:55 +0800

    update: metaname 不支持非法字符画图

--- src/api/canvas-base.ts
+++ src/api/canvas-base.ts
@@ -2,13 +2,9 @@ import i18n from '@/utils/i18n'
 import HttpRequest from '@/utils/request'
 const Canvas = new HttpRequest(`${import.meta.env.VITE_BASEAPI}/canvas-base`, {
   errorHandel: (error: any) => {
-    if (error.response && error.response.data && error.response.data.data !== '') {
-      let message =
-        error?.response?.data?.code === 1
-          ? // @ts-ignore
-            i18n.global.t('Contains unsupported characters')
-          : error.response.data.data
-      return Promise.reject(new Error(message))
+    if (error?.response?.status === 400) {
+      // @ts-ignore
+      return Promise.reject(new Error(i18n.global.t('Contains unsupported characters')))
     } else {
       // 对响应错误做点什么
       return Promise.reject(error)
