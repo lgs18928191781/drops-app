d1e2c09c415ae242cafa0f2a934974cc25d00e04
CallMeSoul <callmesoul@foxmail.com>
2023-03-08 13:49:03 +0800

    add install pwa guide

---
+++ src/components/PWA/PWA.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="pwa main-border primary" v-if="isShow">
+    <div class="header flex flex-align-center">
+      <div class="flex1">
+        <div class="title">{{ $t('PWA.title') }}</div>
+      </div>
+      <a class="close main-border flex flex-align-center flex-pack-center">
+        <Icon name="x_mark" @click="isShow = false" />
+      </a>
+    </div>
+    <div class="content flex">
+      <span class="logo main-border disabled flex flex-align-center flex-pack-center">
+        <Icon name="show" />
+      </span>
+      <div class="flex1">
+        <div class="tips">{{ $t('PWA.tips') }}</div>
+      </div>
+    </div>
+
+    <div class="operate flex flex-pack-end">
+      <a class="main-border" @click="isShow = false">{{ $t('Cancel') }}</a>
+      <a class="main-border" @click="install">{{ $t('PWA.Install') }}</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+const promptEvent: { val: Event | null } = reactive({ val: null })
+const isShow = ref(false)
+
+const i18n = useI18n()
+onMounted(() => {
+  window.addEventListener(
+    'beforeinstallprompt',
+    function(event) {
+      // 监听到可安装事件，进行触发提醒用户
+      event.preventDefault()
+      promptEvent.val = event
+      isShow.value = true
+    },
+    { once: true }
+  )
+
+  window.addEventListener(
+    'appinstalled',
+    () => {
+      i18n.t('PWA.InstallSuccess')
+    },
+    { once: true }
+  )
+})
+
+function install() {
+  // @ts-ignore
+  promptEvent.val?.prompt()
+  isShow.value = false
+  promptEvent.val = null
+}
+</script>
+
+<style lang="scss" scoped src="./PWA.scss"></style>
