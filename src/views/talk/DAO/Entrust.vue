e905ed014de66e8a7f1751a789f4a1f3ebdc5b68
CallMeSoul <callmesoul@foxmail.com>
2023-03-20 17:09:53 +0800

    update: DAO ui

--- src/views/talk/DAO/Entrust.vue
+++ src/views/talk/DAO/Entrust.vue
@@ -1,9 +1,154 @@
 <template>
-  <div>
-    template
+  <div
+    class="h-full overflow-y-auto"
+    v-infinite-scroll="getMore"
+    :infinite-scroll-immediate="false"
+    :infinite-scroll-distance="100"
+  >
+    <div class="header">
+      {{ $t('DAO.Entrust') }}
+    </div>
+
+    <div class="section">
+      <div class="title">{{ $t('DAO.Choose Entrust') }}</div>
+      <div class="content" ref="InputWarpRef">
+        <div class="confirm-user" v-if="userInfo.val">
+          <div class="user flex flex-align-center">
+            <UserAvatar :name="''" :meta-name="''" :meta-id="''" :image="''" />
+            <div class="flex1">
+              <template v-if="userInfo.val!.metaId && userInfo.val!.name">
+                <div class="username">{{ userInfo.val!.name }}</div>
+                <div class="metaid">MetaID: {{ userInfo.val!.metaId.slice(0, 6) }}</div>
+              </template>
+              <template else>
+                <div class="username">Address: {{ userInfo.val!.address }}</div>
+              </template>
+            </div>
+          </div>
+          <div class="operate flex flex-align-center">
+            <a class="main-border flex1 bg-primary">{{ $t('DAO.Confirm Entrust') }}</a>
+            <a class="main-border flex1 faded" @click="userInfo.val = null">{{ $t('Cancel') }}</a>
+          </div>
+        </div>
+        <template v-else>
+          <ElInput v-model="account" placeholder="MetaID/Address/Paymail/MetaName" type="text" />
+          <div class="operate">
+            <div
+              class="main-border flex1"
+              :class="[account ? 'primary' : 'faded']"
+              @click="getUserInfo"
+            >
+              {{ $t('Confirm') }}
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="title">{{ $t('DAO.Entrust Records') }}</div>
+      <div class="content">
+        <ElSkeleton :loading="isSkeleton" animated>
+          <div class="list">
+            <div class="item flex flex-align-center" v-for="(item, index) in records" :key="index">
+              <div class="iten-warp flex1 flex flex-align-center">
+                <div class="user flex flex-align-center">
+                  <UserAvatar :meta-id="''" :name="''" :image="''" :meta-name="''" />
+                  <div class="flex1">
+                    <div class="name">UPKJUI</div>
+                    <div class="metaid">MetaID: 6d5465</div>
+                  </div>
+                </div>
+
+                <Icon name="right" />
+
+                <div class="user flex flex-align-center">
+                  <UserAvatar :meta-id="''" :name="''" :image="''" :meta-name="''" />
+                  <div class="flex1">
+                    <div class="name">UPKJUI</div>
+                    <div class="metaid">MetaID: 6d5465</div>
+                  </div>
+                </div>
+              </div>
+
+              <span class="time flex1">2022-05-11 01:44:13</span>
+            </div>
+
+            <LoadMore :pagination="pagination" />
+          </div>
+        </ElSkeleton>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { initPagination } from '@/config'
+import { getAccountUserInfo, openLoading } from '@/utils/util'
+import { reactive, ref } from 'vue'
+import LoadMore from '@/components/LoadMore/LoadMore.vue'
 
-<style lang="scss" scoped></style>
+const account = ref('')
+const userInfo: { val: UserAllInfo | null } = reactive({
+  val: null,
+})
+const InputWarpRef = ref()
+const records = reactive<any[]>([])
+const pagination = reactive({ ...initPagination })
+const isSkeleton = ref(true)
+
+async function getUserInfo() {
+  if (!account.value) return
+  const loading = openLoading({
+    target: InputWarpRef.value,
+  })
+  const res = await getAccountUserInfo(account.value).catch(error => {
+    ElMessage.error(error.message)
+    loading.close()
+  })
+  if (res) {
+    userInfo.val = res
+    loading.close()
+    account.value = ''
+  }
+}
+
+function getDatas(isCover = false) {
+  return new Promise<void>(async (resolve, reject) => {
+    setTimeout(() => {
+      const res = {
+        code: 0,
+        data: {
+          list: Array.from({ length: 12 }),
+        },
+      }
+      if (res.code === 0) {
+        if (isCover) records.length = 0
+        if (res.data.list.length) {
+          records.push(...res.data.list)
+          pagination.nothing = false
+        } else {
+          pagination.nothing = true
+        }
+
+        resolve()
+      }
+    }, 2000)
+  })
+}
+
+function getMore() {
+  if (pagination.loading || pagination.nothing) return
+  pagination.loading = true
+  pagination.page++
+  getDatas().then(() => {
+    pagination.loading = false
+  })
+}
+
+getDatas(true).then(() => {
+  isSkeleton.value = false
+})
+</script>
+
+<style lang="scss" scoped src="./Entrust.scss"></style>
--- src/views/talk/DAO/proposal/Create.vue
+++ src/views/talk/DAO/proposal/Create.vue
@@ -1,47 +1,60 @@
 <template>
-  <div class="h-full overflow-y-auto overflow-x-hidden">
-    <div class="header flex flex-align-center">
-      <div class="flex1 flex flex-align-center">
-        <a class="back" @click="$router.back()">
-          <Icon name="right" />
-        </a>
-        <span class="title">
-          {{ $t('DAO.New Proposal') }}
-        </span>
+  <div class="h-full overflow-y-auto overflow-x-hidden" ref="WarpRef">
+    <ElAffix :offset="headeroffSetTop">
+      <div class="header flex flex-align-center">
+        <div class="flex1 flex flex-align-center">
+          <a class="back" @click="$router.back()">
+            <Icon name="right" />
+          </a>
+          <span class="title">
+            {{ $t('DAO.New Proposal') }}
+          </span>
+        </div>
+        <a class="main-border primary" @click="submit"> {{ $t('DAO.Publish') }}</a>
       </div>
-      <a class="main-border primary"> {{ $t('DAO.Publish') }}</a>
-    </div>
+    </ElAffix>
 
-    <ElForm :label-position="'top'">
-      <ElFormItem :label="$t('DAO.Proposal Title')" prop="title">
-        <ElInput v-model="form.title" type="text" />
-      </ElFormItem>
-      <ElFormItem :label="$t('DAO.Proposal Type')" prop="type">
-        <ElSelect v-model="form.type">
-          <ElOption
-            v-for="item in typeOptions"
-            :key="item.value"
-            :label="item.name()"
-            :value="item.value"
+    <div class="content">
+      <ElForm :model="form" :label-position="'top'" ref="FormRef" :rules="rules">
+        <ElFormItem :label="$t('DAO.Proposal Title')" prop="title">
+          <ElInput v-model="form.title" type="text" :placeholder="$t('DAO.Enter Proposal Title')" />
+        </ElFormItem>
+        <ElFormItem :label="$t('DAO.Proposal Type')" prop="type">
+          <ElSelect v-model="form.type" @change="onTypeChange">
+            <ElOption
+              v-for="item in typeOptions"
+              :key="item.value"
+              :label="item.name()"
+              :value="item.value"
+            />
+          </ElSelect>
+        </ElFormItem>
+        <ElFormItem :label="$t('DAO.Vote Options')" prop="options">
+          <ElSelect
+            multiple
+            allow-create
+            v-model="form.options"
+            :filterable="true"
+            type="text"
+            :placeholder="$t('DAO.Enter Vote Options')"
+            :disabled="form.type === DAOProposalType.Base"
           />
-        </ElSelect>
-      </ElFormItem>
-      <ElFormItem :label="$t('DAO.Vote Options')" prop="options">
-        <ElSelect multiple allow-create v-model="form.options" :filterable="true" type="text" />
-      </ElFormItem>
-      <ElFormItem :label="$t('DAO.Vote Time')" prop="time">
-        <ElDatePicker
-          v-model="form.time"
-          type="datetimerange"
-          range-separator="To"
-          start-placeholder="Start date"
-          end-placeholder="End date"
-        />
-      </ElFormItem>
-      <ElFormItem :label="$t('DAO.Vote Content')" prop="content">
-        <div class="el-input__wrapper" id="vditor" ref="MarkDownRef"></div>
-      </ElFormItem>
-    </ElForm>
+        </ElFormItem>
+        <ElFormItem :label="$t('DAO.Vote Time')" prop="time">
+          <ElDatePicker
+            v-model="(form.time as any)"
+            type="datetimerange"
+            class="flex1"
+            range-separator="To"
+            :start-placeholder="$t('DAO.Start date')"
+            :end-placeholder="$t('DAO.End date')"
+          />
+        </ElFormItem>
+        <ElFormItem :label="$t('DAO.Vote Content')" prop="content">
+          <div class="el-input__wrapper" id="vditor" ref="MarkDownRef"></div>
+        </ElFormItem>
+      </ElForm>
+    </div>
   </div>
 </template>
 
@@ -51,15 +64,54 @@ import { onMounted, reactive, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import Vditor from 'vditor'
 import 'vditor/dist/index.css'
-const vditor = ref<Vditor | null>(null)
+import { ElAffix, FormInstance, FormRules } from 'element-plus'
 
+const vditor = ref<Vditor | null>(null)
+const headeroffSetTop = ref(0)
+const WarpRef = ref()
 const i18n = useI18n()
 const form = reactive({
   title: '',
   type: DAOProposalType.Base,
-  options: [],
+  options: ['For', 'Against', 'Abstain'],
   time: ['', ''],
 })
+const FormRef = ref<FormInstance>()
+const rules = reactive<FormRules>({
+  title: [{ required: true, message: i18n.t('DAO.Enter Proposal Title'), trigger: 'blur' }],
+  type: [{ required: true, message: i18n.t('DAO.Enter Proposal Type'), trigger: 'blur' }],
+  options: [
+    {
+      required: true,
+      message: i18n.t('DAO.Enter Vote Options'),
+      trigger: 'blur',
+    },
+    {
+      validator: (rule: any, value: any, callback: any) => {
+        if (form.options && form.options.length) {
+          callback()
+        } else {
+          callback(new Error(i18n.t('DAO.Enter Vote Options')))
+        }
+      },
+      trigger: 'blur',
+    },
+  ],
+  time: [
+    {
+      required: true,
+      validator: (rule: any, value: any, callback: any) => {
+        if (form.time && form.time[0] && form.time[1]) {
+          callback()
+        } else {
+          callback(new Error(i18n.t('DAO.Enter Vote Time')))
+        }
+      },
+      trigger: 'blur',
+    },
+  ],
+})
+
 const typeOptions = [
   { name: () => i18n.t('DAO.Basic Type Voting'), value: DAOProposalType.Base },
   { name: () => i18n.t('DAO.Custom Single Choice Voting'), value: DAOProposalType.DiySingleChoose },
@@ -125,8 +177,25 @@ function initMarkDown() {
   })
 }
 
+function onTypeChange() {
+  if (form.type === DAOProposalType.Base) {
+    form.options = ['For', 'Against', 'Abstain']
+  } else {
+    form.options = []
+  }
+}
+
+function submit() {
+  FormRef.value?.validate(async result => {
+    if (result) {
+    }
+  })
+}
+
 onMounted(() => {
   initMarkDown()
+
+  headeroffSetTop.value = WarpRef.value.getBoundingClientRect().top
 })
 </script>
 
