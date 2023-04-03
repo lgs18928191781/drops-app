f5f9ee7ab9e9aabb93921b9e08872f3af855b188
CallMeSoul <callmesoul@foxmail.com>
2023-04-03 17:14:55 +0800

    fix: ui bug

--- vite.config.ts
+++ vite.config.ts
@@ -238,6 +238,7 @@ export default ({ mode, command }) => {
       },
       commonjsOptions: {
         transformMixedEsModules: true,
+        defaultIsModuleExports: true
       },
     },
     sourcemap: isProduction ? false : 'inline',
