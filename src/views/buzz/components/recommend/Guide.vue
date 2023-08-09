a58ffa22ddcd186a5a29c15236faaba7bcdc506f
lgs <lgs18924946920@showpay.top>
2023-08-09 17:01:54 +0800

    fix pdf link

--- src/views/buzz/components/recommend/Guide.vue
+++ src/views/buzz/components/recommend/Guide.vue
@@ -31,10 +31,7 @@ import LearnSVG from '@/assets/svg/guides_icon_learn.svg?url'
 import Question from '@/assets/svg/question.svg?url'
 import Learn from '@/assets/svg/lern.svg?url'
 import { useRouter } from 'vue-router'
-// import FAQ_Zh from '/public/pdf/Show3_FAQ_ZH.pdf'
-// import FAQ_En from '/public/pdf/Show3_FAQ_EN.pdf'
-// import How_To_Use_Zh from '/public/pdf/How_to_Use_Zh.pdf'
-// import How_To_Use_En from '/public/pdf/How_to_Use_En.pdf'
+
 const i18n = useI18n()
 const router = useRouter()
 const guides = [
@@ -60,16 +57,10 @@ const guides = [
     fun: () => {
       window.open(
         i18n.locale.value === 'zh'
-          ? `/public/pdf/How_to_Use_Zh.pdf`
-          : `/public/pdf/How_to_Use_En.pdf`,
+          ? 'https://www.show3.io/新手攻略：如何玩转Show3.pdf'
+          : 'https://www.show3.io/New_User_Onboarding_How_to_Use_Show3.pdf',
         '_blank'
       )
-      // window.open(
-      //   i18n.locale.value === 'zh'
-      //     ? 'https://www.show3.io/新手攻略：如何玩转Show3.pdf'
-      //     : 'https://www.show3.io/New_User_Onboarding_How_to_Use_Show3.pdf',
-      //   '_blank'
-      // )
     },
   },
   {
@@ -77,15 +68,11 @@ const guides = [
     name: () => i18n.t('Buzz.Show3 Faq'),
     fun: () => {
       window.open(
-        i18n.locale.value == 'zh' ? `/public/pdf/Show3_FAQ_ZH.pdf` : `/public/pdf/Show3_FAQ_EN.pdf`,
+        i18n.locale.value === 'zh'
+          ? 'https://www.show3.io/Show3常见问题.pdf'
+          : 'https://www.show3.io/Show3_FAQ.pdf',
         '_blank'
       )
-      // window.open(
-      //   i18n.locale.value === 'zh'
-      //     ? 'https://www.show3.io/Show3常见问题.pdf'
-      //     : 'https://www.show3.io/Show3_FAQ.pdf',
-      //   '_blank'
-      // )
     },
   },
 ]
