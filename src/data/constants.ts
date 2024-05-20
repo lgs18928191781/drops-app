ccde6ae87b439265af2d49acdd9128f9de9bbc0e
lgs <lgs18924946920@showpay.top>
2024-05-20 11:59:29 +0800

    add vspace balance

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,19 +2,86 @@ import { Network } from '@/stores/network'
 import  {type CreateOptions} from '@metaid/metaid'
 export const NETWORK: Network = import.meta.env.VITE_NETWORK || 'regtest'
 
-export type BufferEncoding =
-  | 'ascii'
-  | 'utf8'
-  | 'utf-8'
-  | 'utf16le'
-  | 'utf-16le'
-  | 'ucs2'
-  | 'ucs-2'
-  | 'base64'
-  | 'base64url'
-  | 'latin1'
-  | 'binary'
-  | 'hex'
+
+type EntitySchema = {
+  name: string
+  path:string
+  versions: {
+    version: number
+    body: any[]
+  }[]
+}
+
+export enum BufferEncoding{
+  ascii='ascii',
+  utf8='utf8',
+  ['utf-8']='utf-8',
+  utf16le='utf16le',
+  ['utf-16le']='utf-16le',
+  ucs2= 'ucs2',
+  ['ucs-2']= 'ucs-2',
+  base64= 'base64',
+  base64url='base64url',
+  latin1='latin1',
+  binary='binary',
+  hex='hex'
+}
+  
+
+export const buzzSchema: EntitySchema = {
+    name: 'buzz',
+    path: '/protocols/simplebuzz',
+    versions: [
+      {
+        version: 1,
+        body: [
+          {
+            name: 'content',
+            type: 'string',
+          },
+          {
+            name: 'attachments',
+            type: 'array',
+          },
+        ],
+      },
+    ],
+  }
+
+  export const fileSchema = {
+    name: 'file',
+    nodeName: 'MetaFile',
+    encoding: 'binary',
+    versions: [
+      {
+        version: '1.0.1',
+        body: '',
+      },
+    ],
+  }
+
+  export const likeSchema = {
+    name: 'like',
+    path: '/protocols/payLike',
+    versions: [
+      {
+        version: 1,
+        body: [
+          {
+            name: 'likeTo',
+            type: 'string',
+          },
+          {
+            name: 'isLike',
+            type: 'string',
+          },
+        ],
+      },
+    ],
+  }
+
+
+
 
 
 
--- src/hooks/use-metaid-entity.ts
+++ src/hooks/use-metaid-entity.ts
@@ -1,22 +1,44 @@
 
 import { useConnectionStore } from '@/stores/connection'
 import  {type CreateOptions} from '@metaid/metaid'
-import {type BufferEncoding} from '@/data/constants'
+import { BufferEncoding,fileSchema} from '@/data/constants'
 import { AttachmentItem } from '@/@types/hd-wallet'
+
+
 export type EntityOptions={
-    options:CreateOptions[]
     noBroadcast:'yes' | 'no'
     feeRate?:number
 }
 
+function isEmpty(attachments:AttachmentItem[]){
+    return Array.isArray(attachments) && attachments.length
+}
 
 
 
 
 export function useMetaIDEntity(){
 
-    async function buzzEntity() {
+    const connectStore = useConnectionStore()
+
+    
+
+    async function buzzEntity(body:{content:string,attachments:AttachmentItem[] | []}) {
+        const buzzEntity =connectStore.last.use('buzz')
+        const finalBody:any = body.content
         
+        if(!isEmpty(body.attachments)){
+            const imageRes= await fileEntity(body.attachments)
+            finalBody.attachments=imageRes.revealTxIds.map(rid=>{
+                return `metafile://${rid}i0`
+            })
+        }
+
+        const createRes =(await buzzEntity).create({
+            options: [{ body: JSON.stringify(finalBody) }],
+		    noBroadcast:'no',
+        })
+        return createRes
     }
 
 
@@ -33,16 +55,16 @@ export function useMetaIDEntity(){
         for(const image of images){
             fileOptions.push({
                 body:Buffer.from(image.data, "hex").toString("base64"),
-                encoding: 'base64' as BufferEncoding ,
+                encoding: BufferEncoding.base64,
                 contentType:image.fileType,
                 toEncodeing:fileSchema.encoding
             })
         }
-        const connectStore = useConnectionStore()
+        
         const fileEntity=await connectStore.last.use('file')
         const imageRes=await fileEntity.create({
             options: fileOptions,
-		    noBroadcast: "no",
+		    noBroadcast: 'no',
         })
 
         return imageRes
@@ -50,7 +72,8 @@ export function useMetaIDEntity(){
    }
 
    return{
-    fileEntity
+    fileEntity,
+    buzzEntity
    }
 
 
